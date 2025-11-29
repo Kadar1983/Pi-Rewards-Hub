d8b3398fc3fb5f002c339804e370574cccccafde
Kadar1983 <kadar.zh@gmail.com>
2025-11-29 18:00:17 +0100

    Update and rename login.html to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PiRewardsHub from "./PiRewardsHub";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<PiRewardsHub />);
+
+
+---
