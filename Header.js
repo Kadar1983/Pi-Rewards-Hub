b50563b4b8782e72ec31dacf90490b41a7809d79
Kadar1983 <kadar.zh@gmail.com>
2025-11-30 19:17:42 +0100

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header style={{ padding: "10px", backgroundColor: "#0b1220", color: "#fff" }}>
+      <h1>Pi Rewards Hub</h1>
+    </header>
+  );
+}
