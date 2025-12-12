8995138164cb2bd5ee69fd8e9adc43f517746808
Kadar1983 <kadar.zh@gmail.com>
2025-12-12 22:55:51 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}"
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
   ],
-  darkMode: "media",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        piPurple: {
+          50: "#f6f5ff",
+          100: "#ede9fe",
+          500: "#7c3aed",
+          700: "#5b21b6"
+        }
+      },
+      borderRadius: {
+        xl2: "1rem"
+      }
+    },
   },
   plugins: [],
-};
+  darkMode: 'class'
+}
