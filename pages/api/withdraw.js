29f201ca4d8c31e16e44efd0e04f4d4ba8bd18cb
Kadar1983 <kadar.zh@gmail.com>
2025-12-12 21:55:12 +0100

    Update withdraw.js

--- pages/api/withdraw.js
+++ pages/api/withdraw.js
@@ -1,13 +1,11 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
     const { amount } = req.body;
-
     if (!amount || isNaN(amount) || amount <= 0) {
       return res.status(400).json({ message: "Invalid amount" });
     }
-
     return res.status(200).json({
-      message: `Withdrawal request for ${amount} Pi has been received!`,
+      message: `Withdrawal request for ${amount} Pi has been received!`
     });
   } else {
     res.setHeader("Allow", ["POST"]);
