6334c9910b25be4f1051a0857697a57ce86b3bfa
Kadar1983 <kadar.zh@gmail.com>
2025-12-13 19:16:46 +0100

    Delete components/Card.js

--- components/Card.js
+++
@@ -1,8 +0,0 @@
-export default function Card({ title, children }) {
-  return (
-    <div className="bg-white/10 p-4 rounded-2xl">
-      <h2 className="font-semibold mb-2">{title}</h2>
-      {children}
-    </div>
-  );
-}
