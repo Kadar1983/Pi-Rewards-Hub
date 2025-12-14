c332e9d1284d1ab160d2ce1e921e7689e353d991
Kadar1983 <kadar.zh@gmail.com>
2025-12-14 18:53:33 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import { useEffect } from "react";
+import Head from "next/head";
 import "../styles/globals.css";
-import { initPi } from "../lib/pi";
 
-export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    initPi();
-  }, []);
-
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <script src="https://sdk.minepi.com/pi-sdk.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
