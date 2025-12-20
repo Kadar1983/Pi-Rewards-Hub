3d13aafefa905e2ae32857c219027fa0c7169e68
Kadar1983 <kadar.zh@gmail.com>
2025-12-20 17:19:25 +0100

    Update PiAuth.js

--- components/PiAuth.js
+++ components/PiAuth.js
@@ -1,7 +1,6 @@
 export const PiAuth = () => {
   return new Promise((resolve, reject) => {
     if (!window.Pi) return reject("Pi SDK not loaded. Open in Pi Browser.");
-
     window.Pi.authenticate(
       ["username"],
       (auth) => resolve(auth.user),
