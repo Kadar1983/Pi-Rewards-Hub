7bc1b7d97fd097513a30f0eb66304297756fbb7d
Kadar1983 <kadar.zh@gmail.com>
2025-12-20 17:06:30 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}"
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        gold: '#FFD700',
-        violet: '#8B5CF6',
-        darkbg: '#0F0F0F'
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
