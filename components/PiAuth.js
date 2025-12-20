3d6875bb3d0f6589c5d311a0f607798d9cc0dd96
Kadar1983 <kadar.zh@gmail.com>
2025-12-20 17:04:30 +0100

    Update PiAuth.js

--- components/PiAuth.js
+++ components/PiAuth.js
@@ -1,17 +1,11 @@
-export default function PiAuth() {
-  const login = async () => {
-    if (!window.Pi) return alert("Open in Pi Browser");
-    try {
-      const user = await window.Pi.authenticate(["username"], () => {});
-      alert("Welcome " + user.user.username);
-    } catch {
-      alert("Login failed");
-    }
-  };
+export const PiAuth = () => {
+  return new Promise((resolve, reject) => {
+    if (!window.Pi) return reject("Pi SDK not loaded. Open in Pi Browser.");
 
-  return (
-    <button onClick={login} className="btn-gold w-full mt-3">
-      Login with Pi
-    </button>
-  );
-  }
+    window.Pi.authenticate(
+      ["username"],
+      (auth) => resolve(auth.user),
+      (err) => reject(err)
+    );
+  });
+};
