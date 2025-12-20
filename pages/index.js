0491850b4fee50e616bed7e60d8c6af871e39c7a
Kadar1983 <kadar.zh@gmail.com>
2025-12-20 16:46:46 +0100

    Refactor dynamic import of Home component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import dynamic from "next/dynamic";
 
-const Home = dynamic(() => import("../components/Home"), {
-  ssr: false,
-});
+const Home = dynamic(() => import("../components/Home"), { ssr: false });
 
 export default Home;
