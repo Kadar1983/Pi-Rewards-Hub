bd4f0bbe9040b4ed009971eec435207e08c04d43
Kadar1983 <kadar.zh@gmail.com>
2025-12-21 18:22:01 +0100

    Update withdraw.js

--- pages/withdraw.js
+++ pages/withdraw.js
@@ -1,10 +1,7 @@
-import Home from "../components/Home";
-
 export default function Withdraw() {
   return (
-    <div>
-      <h1 className="text-center text-3xl font-bold my-6">💰 Withdraw</h1>
-      <Home />
+    <div className="min-h-screen flex items-center justify-center text-3xl font-bold">
+      💰 Withdraw Page
     </div>
   );
 }
