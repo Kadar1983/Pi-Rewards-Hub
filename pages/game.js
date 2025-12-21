da763e9f130ff046cba7baed6b0ef7eb0246582a
Kadar1983 <kadar.zh@gmail.com>
2025-12-21 18:20:42 +0100

    Update game.js

--- pages/game.js
+++ pages/game.js
@@ -1,10 +1,7 @@
-import Home from "../components/Home";
-
 export default function Game() {
   return (
-    <div>
-      <h1 className="text-center text-3xl font-bold my-6">🎮 Game</h1>
-      <Home />
+    <div className="min-h-screen flex items-center justify-center text-3xl font-bold">
+      🎮 Game Page
     </div>
   );
 }
