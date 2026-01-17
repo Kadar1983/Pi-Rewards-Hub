43e41527d3cf52fccaf1a3627062d9fc0d3dacaa
Kadar1983 <kadar.zh@gmail.com>
2026-01-17 15:30:23 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Navbar from "../components/Navbar";
 export default function App({ Component, pageProps }) {
   return (
     <AppProvider>
-      <div style={{ paddingBottom: 70, maxWidth: 400, margin: "0 auto" }}>
+      <div style={{ paddingBottom: 80, maxWidth: 400, margin: "0 auto" }}>
         <Component {...pageProps} />
       </div>
       <Navbar />
