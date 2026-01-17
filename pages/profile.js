748d78df06c066c9676fa66bd24f7dc20eb503e8
Kadar1983 <kadar.zh@gmail.com>
2026-01-17 15:35:00 +0100

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -1,17 +1,16 @@
 import { useApp } from "../components/AppContext";
 export default function Profile() {
-  const { points, user } = useApp();
+  const { user, points } = useApp();
+
   return (
-    <div style={{ padding: 20, textAlign: "center" }}>
+    <div style={{ textAlign: "center", padding: 20 }}>
       <h2>🧑‍💻 الملف الشخصي</h2>
       {user ? (
         <>
           <p>المستخدم: {user}</p>
           <p>النقاط: {points}</p>
         </>
-      ) : (
-        <p>يرجى تسجيل الدخول أولًا</p>
-      )}
+      ) : <p>يرجى تسجيل الدخول أولًا</p>}
     </div>
   );
-}
+        }
