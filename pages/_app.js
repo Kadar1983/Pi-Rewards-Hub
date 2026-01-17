8fdb4778e62048771ae2b1147203698e441f3e80
Kadar1983 <kadar.zh@gmail.com>
2026-01-17 15:15:22 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import { AppProvider } from "../components/AppContext";
+import Navbar from "../components/Navbar";
 
 export default function App({ Component, pageProps }) {
   return (
     <AppProvider>
-      <Component {...pageProps} />
+      <div style={{ paddingBottom: 70, maxWidth: 400, margin: "0 auto" }}>
+        <Component {...pageProps} />
+      </div>
+      <Navbar />
     </AppProvider>
   );
 }
