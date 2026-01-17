01b00965c8f11bc779699a4102444227d683bfa1
Kadar1983 <kadar.zh@gmail.com>
2026-01-17 17:17:16 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
-import { AppProvider } from "../components/AppContext";
 import "../styles/globals.css";
+import { AppProvider } from "../components/AppContext";
+import Navbar from "../components/Navbar";
 
 export default function App({ Component, pageProps }) {
   return (
     <AppProvider>
+      <Navbar />
       <Component {...pageProps} />
     </AppProvider>
   );
