6df11fa1f0c76acaa6600c18c019d1bf840173d9
Kadar1983 <kadar.zh@gmail.com>
2026-01-17 15:48:30 +0100

    Update game.js

--- pages/game.js
+++ pages/game.js
@@ -1,4 +1,8 @@
-import Game from "../components/Game";
-export default function GamePage() {
-  return <Game />;
-    }
+import { useApp } from "./AppContext";
+
+// داخل اللعبة
+const { user, addPoints } = useApp();
+
+// عند الضغط على كائن
+if (!user) return alert("يرجى تسجيل الدخول أولًا");
+addPoints(1); // النقاط ستضاف لحساب المستخدم
